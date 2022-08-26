9d469c1d778dc46889e15d4b30fb0d2c8e9898b7
Andrey Soktoev <andrey.soktoev@gmail.com>
2022-08-26 21:51:35 +0800

    db

--- erp.aero/app.js
+++ erp.aero/app.js
@@ -1,7 +1,13 @@
 import fastify from 'fastify'
+import postgres from 'postgres'
 
 export function app() {
   const f = fastify()
+
+  f.decorate('sql', postgres({
+    username: 'postgres',
+    password: 'postgres'
+  }))
 
   return f
 }
---
+++ erp.aero/db.sql
@@ -0,0 +1,11 @@
+select 'create database erp_aero'
+where not exists (select from pg_database where datname = 'erp_aero')
+\gexec
+\c erp_aero postgres
+
+drop table if exists users cascade;
+
+create table users (
+  id text unique not null,
+  password text not null
+);
