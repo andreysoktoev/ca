679a6773a62c82441888090474d5e96b75abc659
Andrey Soktoev <andrey.soktoev@gmail.com>
2022-09-07 02:15:34 +0800

    outside-digital: init

---
+++ outside-digital/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.cjs'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'eol-last': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always', { 'arraysInObjects': true }],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true, 'avoidEscape': true }],
+    'semi': ['error', 'never'],
+    'template-curly-spacing': ['error', 'never']
+  },
+};
