17c452faae68e3faad3eef5b727533fbf5c3b88d
Andrey Soktoev <andrey.soktoev@gmail.com>
2022-09-08 04:00:12 +0800

    class-validator

--- outside-digital/src/users/dto/create-user.dto.ts
+++ outside-digital/src/users/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsOptional, MaxLength, MinLength } from 'class-validator'
+
 export class CreateUserDto {
+  @IsEmail()
+  @MaxLength(100)
   email: string
+
+  @MinLength(8)
+  @MaxLength(100)
   password: string
+
+  @MaxLength(30)
   nickname: string
 }
 
@@ -10,7 +19,17 @@ export class Credentials {
 }
 
 export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(100)
   email?: string
+
+  @IsOptional()
+  @MinLength(8)
+  @MaxLength(100)
   password?: string
+
+  @IsOptional()
+  @MaxLength(30)
   nickname?: string
 }
